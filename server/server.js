e9fbe82076093b030e39da4637e3512e175bf07b
CWMyster <infiniteaxis@gmail.com>
2017-11-29 21:21:02 -0600

    second

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const Weather = require('../modules/weather');
 const Hapi = require('hapi');
-const server = new Hapi.Server({ host: 'localhost', port: 3000 });
+const server = new Hapi.Server({ host: 'https://node-weather-app7653.herokuapp.com/', port: 8080 });
 
 server.start()
   .then(() =>  { console.log(`Listening on ${server.info.uri}`)})
